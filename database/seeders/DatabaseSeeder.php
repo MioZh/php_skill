fea91b540a3e72c33ee73a9f6706c199c1623ec4
Azamat Zholdasov <MioZh>
2024-02-26 21:08:11 +0600

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Skill;
+use App\Models\UserSkill;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,7 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //User::factory(10)->create();
+        //SKill::factory(10)->create();
+        //UserSkill::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
